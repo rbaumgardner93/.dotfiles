1bba1a645ecd74678d00c8526f9661cf1613bf7f
Ryan Baumgardner <rbaumgardner@planview.com>
2024-10-28 22:48:20 -0400

    fix: updates to startup script

--- startup.sh
+++ startup.sh
@@ -2,12 +2,33 @@
 
 set -e
 
+function add_ssh_key() {
+	PUB_SSH=~/.ssh/id_ed25519.pub
+	if [ -f $PUB_SSH ]; then
+		printf "Skipping SSH key generation key already exists...\n"
+	else
+		read -p "Please enter email used for github: " GITHUB_EMAIL
+		ssh-keygen -t ed25519 -C "$GITHUB_EMAIL"
+	fi
+
+	printf "Add ssh to github before continuing, see: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account /n"
+
+	read -p "Continue? " -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+	    printf "Continuing the install script...\n"
+	fi
+}
+
 function install_brew() {
 	if type brew &> /dev/null; then
 		printf "Skipping brew install brew already installed...\n"
 	else
 		printf "Installing brew...\n"
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		printf "Adding brew to PATH...\n"
+		echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+		eval "$(/opt/homebrew/bin/brew shellenv)"
 	fi
 
 	if type git &> /dev/null; then
@@ -42,6 +63,7 @@ function source_zsh() {
 	source ~/.zshrc
 }
 
+add_ssh_key
 install_brew
 install_dotfiles
 install_brew_packages
